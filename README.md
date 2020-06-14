65d8ffde03df54986c4e27250d0669bb48200a3f
Michael Williams <mwilliams2964@gmail.com>
2020-06-14 15:56:07 -0700

    added content to readme to describe how to run app on local machine

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # myPersonalPraject
+
+## In order to run the app locally,
+1. Open your Terminal and **cd** into the **server** folder of the project repo.
+2. Run **npm start**.
+3. Open the project repo in VS Code.
+4. From your VS Code Terminal, **cd** into the **client** folder.
+5. From here run **npm start**. Now your project should be viewable in your browser at **http://localhost:3000/**.
+
+*note: the server is listening on port 5000. The frontend makes requests on port 3000. A proxy has been set up in the frontend package.json which references port 5000 so that it can receive information from the server.*
--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -5,21 +5,21 @@ const Weather = () => {
 
     const [message, setMessage] = useState('');
 
-    useEffect(() => {
-        fetch.get("")
-        .then(response =>  {
-            // console.log("response: ", response);
-            // console.log("response.json(): ",response.json());
-            // return response.json();
-            return response.text();
-        })
-        .then(data => {
-            setMessage(data)
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-          })
+    useEffect(async () => {
+        const helloWorld = await fetch.get("")
+            .then(response => {
+                // console.log("response: ", response);
+                // console.log("response.json(): ",response.json());
+                // return response.json();
+                return response.text();
+            })
+            .then(data => {
+                setMessage(data)
+                console.log(data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            })
     }, []);
 
     return (
@@ -30,3 +30,28 @@ const Weather = () => {
 }
 
 export default Weather;
+
+// function deathStats (current, beforeCovid) {
+//     const deathRate = (1 - (current / beforeCovid)) * 100;
+//     console.log(`Death Rate: ${deathRate}%`);
+// };
+
+// // or
+
+// function deathStats2 (confirmedDeathToll, nycPopBeforeCovid) {
+//     const currentPop = nycPopBeforeCovid - confirmedDeathToll;
+//     const deathRate = (1 - (currentPop / nycPopBeforeCovid)) * 100;
+//     console.log(`Death Rate: ${deathRate}%`);
+// };
+
+// function deathStats3 (confirmedDeathToll, nycPopBeforeCovid) {
+//     const deathRate = (confirmedDeathToll / nycPopBeforeCovid) * 100;
+//     const errorRange = 0.5/100; // 0.5% as a number
+//     const highestCensusPopEstimate = nycPopBeforeCovid + (nycPopBeforeCovid * errorRange);
+//     const lowestCensusPopEstimate = nycPopBeforeCovid - (nycPopBeforeCovid * errorRange);
+//     const deathRateHigh = (confirmedDeathToll / lowestCensusPopEstimate) * 100;
+//     const deathRateLow = (confirmedDeathToll / highestCensusPopEstimate) * 100;
+//     console.log(`Death Rate Low: ${deathRateLow.toFixed(4)}`);
+//     console.log(`Death Rate middle: ${deathRate.toFixed(4)}%`);
+//     console.log(`Death Rate High: ${deathRateHigh.toFixed(4)}`);
+// };
