6ca8d9d651c273ea1ba331ff1b61fcd6c8884aad
Michael Williams <mwilliams2964@gmail.com>
2021-02-06 15:30:16 -0700

    changed directory structure to include pages directory

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import HelloWorld from "../components/HelloWorld";
+import Counter from "../components/Counter";
+import logo from "../logo.svg";
+import "../App.css";
+
+function Homepage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Counter />
+
+        <HelloWorld />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Homepage;
