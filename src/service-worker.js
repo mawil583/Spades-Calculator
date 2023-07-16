11f1554e2a4faec556d95d3bb31fb31830af3173
Michael Williams <michaelwilliams@Michaels-MacBook-Pro.local>
2023-07-16 16:26:56 -0700

    added urlsToCache

--- src/service-worker.js
+++ src/service-worker.js
@@ -71,7 +71,7 @@ self.addEventListener('message', (event) => {
 
 // Any other custom service worker logic can go here.
 const CACHE_NAME = "cache_sample";
-const urlsToCache = ["index.html", "offline.html", "https://spadescalculator.com/spades-calculator", "https://spadescalculator.com/"];
+const urlsToCache = ["index.html", "offline.html", "https://spadescalculator.com/spades-calculator", "https://spadescalculator.com/", "/", "/css/style.css", "/js/main.js"];
 const version = "v0.0.1";
 //install sw at first time
 //place to cache assets to speed up the loading time of web page
